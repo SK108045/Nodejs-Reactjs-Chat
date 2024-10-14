c57c0a39809e8df5b9884fceadd79581048e2d51
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-10-13 23:34:18 -0400

    Add PORT variable
    Use the environment's PORT variable, or fallback to 3001 if it's not set

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios"); // Require axios module
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
